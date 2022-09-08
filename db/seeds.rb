8a41c64c3bfa2a826ed5af25e6961590ea00d792
NondasNF <nondasnoronha@gmail.com>
2022-09-08 14:34:48 -0300

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Seeding database..."
+
+# Create ALBUMS
+
+puts "Creating albums..."
+
+Album.create!([
+  { name: "The Dark Side of the Moon", price: 10.99 },
+  { name: "The Wall", price: 12.99 },
+  { name: "Wish You Were Here", price: 11.99 },
+  { name: "Animals", price: 9.99 },
+  { name: "The Final Cut", price: 8.99 },
+  { name: "The Division Bell", price: 9.99 }
+])
+
+puts "Done! #{Album.count} albums created."
+
