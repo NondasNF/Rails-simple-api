50cb8be5a0fc84d3cee879e4239ff4cd610ef155
NondasNF <nondasnoronha@gmail.com>
2022-09-08 15:22:54 -0300

    set controller and routes to api directory

---
+++ app/controllers/api/v1/albums_controller.rb
@@ -0,0 +1,41 @@
+  class Api::V1::AlbumsController < ApplicationController
+    
+    def index
+      @albums = Album.all
+      render :json => @albums
+    end
+
+    def show
+      render :json => @album
+    end
+
+    def create
+      if @album.save
+        render :json => @album, :status => :created, :location => @album
+      else
+        render :json => @album.errors, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      if @album.update_attributes(params[:album])
+        head :no_content
+      else
+        render :json => @album.errors, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @album.destroy
+      head :no_content
+    end
+
+    private
+    def set_album
+      @album = Album.find(params[:id])
+    end
+
+    def album_params
+      params.require(:album).permit(:name, :price)
+    end
+  end
